17a1d138b73394abc5cc030e12c3fc505852f333
Sebastian Velandia <jsvelandiab@uqvirtual.edu.co>
2021-03-16 17:15:23 -0500

    GET,PUT,POST,DELETE, calificacion

--- Projects/Django/DjangoAPI/HistoriaApp/urls.py
+++ Projects/Django/DjangoAPI/HistoriaApp/urls.py
@@ -4,6 +4,13 @@ from HistoriaApp import views
 urlpatterns = [
     url(r'^autor/$',views.autorApi),
     url(r'^autor/([0-9]+)$',views.autorApi),
+
     url(r'^libro/$',views.libroApi),
-    url(r'^libro/([0-9]+)$',views.libroApi)
+    url(r'^libro/([0-9]+)$',views.libroApi),
+
+    url(r'^resena/$',views.reseñaApi),
+    url(r'^resena/([0-9]+)$',views.reseñaApi),
+
+    url(r'^calificacion/$',views.calificacionApi),
+    url(r'^calificacion/([0-9]+)$',views.calificacionApi)
 ]
